f0423ae0cc77f83fe862a824d2db15c63c65ee80
Marcelo Augusto <mrcelo014@gmail.com>
2024-01-26 17:17:22 -0300

    test failed

---
+++ setup/src/app/about/__test__/initial.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+import About from '../page'
+
+describe('test', () => {
+  it('test', () => {
+    expect(1).toBe(1)
+    render(<About />)
+  })
+})
